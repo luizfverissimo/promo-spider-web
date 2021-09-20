9a80edd48144184133b5f8f1e707163f7c9bb0ed
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-09-20 18:23:10 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
+          <a className="text-red-900" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
