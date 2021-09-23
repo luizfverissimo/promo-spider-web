a8ad3d48942316b0c1c241117d949ff35df33320
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-09-23 01:20:54 -0300

    Update firebaseNode.js

--- services/firebaseNode.js
+++ services/firebaseNode.js
@@ -5,7 +5,7 @@ if(!admin.apps.length) {
     credential: admin.credential.cert({
       "project_id": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_ADMIN,
       "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-      "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\n/g, '\n'),
       "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
     }),
     databaseURL: process.env.FIREBASE_DATABASE_URL
