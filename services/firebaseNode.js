a876225d852d7e9a513ba3ce08c2733359fd4711
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-09-23 00:33:25 -0300

    Update firebaseNode.js

--- services/firebaseNode.js
+++ services/firebaseNode.js
@@ -3,9 +3,9 @@ import admin from 'firebase-admin';
 if(!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_ADMIN,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, '\n')
+      "project_id": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_ADMIN,
+      "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+      "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, '\n')
     }),
     databaseURL: process.env.FIREBASE_DATABASE_URL
   });
