2059a9ce1007a6993118e4a291cf80b0e1637195
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-09-26 14:16:53 -0300

    feat(gamerOffers): add channel logic to show offers

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import admin from '../services/firebaseNode';
 import Card from '../components/Card';
 import Header from '../components/Header';
 
-export default function Home({ macOffersData, promoToolsData }) {
+export default function Home({ macOffersData, promoToolsData, gamerOffersData }) {
   return (
     <div className='w-screen min-h-screen bg-theme-white flex flex-col items-center'>
       <Head>
@@ -20,7 +20,7 @@ export default function Home({ macOffersData, promoToolsData }) {
           <div className='w-full flex justify-between items-center mt-9 flex-col  md:flex-row'>
             <div className='w-full flex flex-col items-center mb-4 md:mb-0 md:items-start'>
               <p className='font-archivo font-normal text-lg text-theme-gray'>
-                MacOfertas
+                🍎 MacOfertas
               </p>
               <h2 className='font-epilogue font-bold text-2xl text-theme-black'>
                 Todas as ofertas
@@ -47,14 +47,41 @@ export default function Home({ macOffersData, promoToolsData }) {
           <div className='w-full flex justify-between items-center mt-9 flex-col  md:flex-row'>
             <div className='w-full flex flex-col items-center mb-4 md:mb-0 md:items-start'>
               <p className='font-archivo font-normal text-lg text-theme-gray'>
-                Ferramenta Barata
+                🎮 GamerOfertas
               </p>
               <h2 className='font-epilogue font-bold text-2xl text-theme-black'>
                 Todas as ofertas
               </h2>
             </div>
             <a
-              href='https://t.me/macofertas'
+              href='https://t.me/gamerofertas'
+              target='_blank'
+              re-='noopener noreferrer'
+              className='flex justify-center items-center bg-theme-gray text-theme-white px-4 py-3 rounded-xl whitespace-nowrap font-epilogue font-bold text-theme-white cursor-pointer transition-all hover:brightness-110 gap-2'
+            >
+              Entrar para o grupo do Telegram <FaTelegramPlane />
+            </a>
+          </div>
+
+          <div className='w-full flex flex-wrap justify-around mt-11 gap-6'>
+            {gamerOffersData.map((offer) => (
+              <Card key={offer.id} data={offer} />
+            ))}
+          </div>
+        </section>
+
+        <section className='w-full flex flex-col items-center'>
+          <div className='w-full flex justify-between items-center mt-9 flex-col  md:flex-row'>
+            <div className='w-full flex flex-col items-center mb-4 md:mb-0 md:items-start'>
+              <p className='font-archivo font-normal text-lg text-theme-gray'>
+                🔧 Ferramenta Barata
+              </p>
+              <h2 className='font-epilogue font-bold text-2xl text-theme-black'>
+                Todas as ofertas
+              </h2>
+            </div>
+            <a
+              href='https://t.me/ferramentaBarata'
               target='_blank'
               re-='noopener noreferrer'
               className='flex justify-center items-center bg-theme-gray text-theme-white px-4 py-3 rounded-xl whitespace-nowrap font-epilogue font-bold text-theme-white cursor-pointer transition-all hover:brightness-110 gap-2'
@@ -78,20 +105,27 @@ export const getServerSideProps = async () => {
   const db = admin.firestore();
   let macOffersData = [];
   let promoToolsData = [];
+  let gamerOffersData = [];
 
-  const snapshotMacOffers = await db.collection('macoffer').limit(20).get();
+  const snapshotMacOffers = await db.collection('macoffer').limit(8).get();
   snapshotMacOffers.forEach((item) => {
     const data = item.data();
     macOffersData.push(data);
   });
 
-  const snapshotPromoTools = await db.collection('promotools').limit(20).get();
+  const snapshotPromoTools = await db.collection('promotools').limit(8).get();
   snapshotPromoTools.forEach((item) => {
     const data = item.data();
     promoToolsData.push(data);
   });
 
+  const snapshotGamerOffers = await db.collection('gameroffers').limit(8).get();
+  snapshotGamerOffers.forEach((item) => {
+    const data = item.data();
+    gamerOffersData.push(data);
+  });
+
   return {
-    props: { macOffersData, promoToolsData }
+    props: { macOffersData, promoToolsData, gamerOffersData }
   };
 };
